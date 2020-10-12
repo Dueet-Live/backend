bf027d7e6cbd3fc955d2d6c51b6345dc90f0d356
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-13 00:00:09 +0800

    Use HTTPS server

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,5 +1,16 @@
-import io from 'socket.io';
+import fs from 'fs';
+import https from 'https';
 import { Server } from './server';
 
-export const ioServer = io(process.env.PORT || 3000);
-export const server = new Server(ioServer);
+const port = process.env.PORT || 3000;
+const key = process.env.KEY || './key.pem';
+const cert = process.env.KEY || './cert.pem';
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync(key),
+  cert: fs.readFileSync(cert),
+});
+const gameServer = new Server(httpsServer);
+
+gameServer.start(port);
+console.log(`Server started on port ${port}`);
