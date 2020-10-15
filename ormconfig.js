71e1b3e130e513a2da9fb436a724f57f3b6e7e0b
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-15 22:59:02 +0800

    Remove migrations and synchronize automatically

--- ormconfig.js
+++ ormconfig.js
@@ -2,14 +2,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+// TODO: need migrations?
 module.exports = {
   type: 'postgres',
   url: process.env.DB_URL,
-  synchronize: false,
+  synchronize: true,
   logging: true,
   entities: ['dist/db/entities/**/*.js'],
-  migrations: ['dist/db/migrations/**/*.js'],
-  cli: {
-    migrationsDir: 'src/db/migrations/',
-  },
 };
